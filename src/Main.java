20fae48d2c79e0ca236652fa9505bf1cc8aed075
mordar-20 <55185628+mordar-20@users.noreply.github.com>
2019-12-04 10:50:57 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ public class Main {
         System.out.println("Hallo Elco2000");
         System.out.println("Hallo Elco Mussert");
         System.out.println("Hello World!");
+        System.out.println("Hello joris!");
     }
 
     public static void sayHello() {
