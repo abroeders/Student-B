e31f70340b862762df7f4631b9a216ac399ad652
elco2000 <elcomussert@gmail.com>
2019-12-04 10:46:31 +0100

    Change main text

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Hallo Elco");
+        System.out.println("Hallo Elco Mussert");
         System.out.println("Hello World!");
     }
 
